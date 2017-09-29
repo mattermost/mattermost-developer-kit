024bbb52c14051b4c2239bca0d0893104c78e8ca
JoramWilander <jwawilander@gmail.com>
2017-09-29 10:12:39 -0400

    Bump version

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 var program = require('commander');
 
 program
-    .version('0.0.5')
+    .version('0.0.6')
     .command('init', 'Initialize an integration or plugin template')
     .parse(process.argv);
